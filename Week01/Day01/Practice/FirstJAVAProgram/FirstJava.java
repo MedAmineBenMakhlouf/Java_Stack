3a791a767d018cd1a8c3aa3a5373413c91895ee2
Amine <mohamedamin.benmakhlouf@gmail.com>
2023-10-02 14:07:17 +0100

    First commit

---
+++ Week01/Day01/Practice/FirstJAVAProgram/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My name is Amine\nI am 35 years old\nMy Hometown is Tunisia, Tunis ");
+    }
+}
